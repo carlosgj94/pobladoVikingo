f077843a752234554fdacc3710347b87577b14a3
Iulen <im_993@hotmail.com>
2016-05-06 20:29:12 +0200

    Creado el prefab de la gestión de variables

---
+++ vegvisir/Assets/VariableManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariableManager : MonoBehaviour {
+
+	public int sangre = 0;
+	public int hambre = 0;
+	public int fiesta = 0;
+	public int poblacion = 1000;
+	public int dias = 1;
+
+	public void aumentarSangre(int cantidad){
+		sangre += cantidad;
+	}
+	public void disminuirSangre(int cantidad){
+		sangre -= cantidad;
+	}
+
+
+	public void aumentarHambre(int cantidad){
+		hambre += cantidad;
+	}
+	public void disminuirHambre(int cantidad){
+		hambre -= cantidad;
+	}
+
+
+	public void aumentarFiesta(int cantidad){
+		fiesta += cantidad;
+	}
+	public void disminuirFiesta(int cantidad){
+		fiesta -= cantidad;
+	}
+
+
+	public void aumentarPoblacion(int cantidad){
+		poblacion += cantidad;
+	}
+	public void disminuirPoblacion(int cantidad){
+		poblacion -= cantidad;
+	}
+
+	public void variablesNuevoDia(){
+		
+		int random = Random.Range (0,2);
+
+		if (random == 0){
+			sangre++;
+		}else if(random == 1){
+			hambre++;
+		}else if(random == 2) {
+			fiesta++;
+		}
+
+		int variacionPoblacion = Mathf.Round((poblacion * Random(-5,5))/100);
+		poblacion += variacionPoblacion;
+
+	}
+
+}
