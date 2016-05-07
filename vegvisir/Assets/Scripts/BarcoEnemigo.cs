f66fd87241a70eb26b2746250249efc289b17d81
Iulen <im_993@hotmail.com>
2016-05-07 20:48:38 +0200

    Minijuego barcos V1

---
+++ vegvisir/Assets/Scripts/BarcoEnemigo.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BarcoEnemigo : MonoBehaviour {
+
+	private GameObject barcoJugador;
+
+	//private Vector3 velocity = Vector3.zero;
+
+	private Vector3 startPosition; 
+	private float startTime;
+	private float speed = 2.2F;
+	private float journeyLength;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+		startPosition = transform.position;
+		journeyLength = Vector3.Distance (startPosition, new Vector3 (0, 0, 0));
+		barcoJugador = GameObject.FindGameObjectWithTag("BarcoVikingo");   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//this.transform.position = Vector3.SmoothDamp (this.transform.position, new Vector3 (0, 0, 0), ref velocity, 4f);
+
+		float distCovered = (Time.time - startTime) * speed;
+		float fracJourney = distCovered / journeyLength;
+		transform.position = Vector3.Lerp (startPosition, new Vector3 (0, 0, 0), fracJourney);
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+		if (coll.gameObject.tag == "Lanza") {
+			Destroy (coll.gameObject);
+			Destroy (gameObject);
+			barcoJugador.GetComponent<juegoBarcos>().aumentarPuntuacion();
+
+		}
+	}
+		
+}
+
+
---
+++ vegvisir/Assets/Scripts/juegoBarcos.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class juegoBarcos : MonoBehaviour {
+
+
+
+	private float momment;
+	private bool acabado = false;
+
+	public GameObject cartel;
+	public Text puntuacionFinal;
+	public Text textoVidas;
+	public Text textoPuntuacion;
+
+	public GameObject barcoEnemigo1, barcoEnemigo2;
+
+	private int vidas = 3;
+	public int puntuacion = 0;
+
+	// Use this for initialization
+	void Start () {
+		momment = -0.4f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!acabado) {
+			if (Time.time - momment > 1.5) {
+				momment = Time.time;
+				int random = Random.Range (0, 4);
+				if (random == 0) {
+					Instantiate (barcoEnemigo1, new Vector3 (-11, 6.5f, 0), Quaternion.identity);
+				}
+				if (random == 1) {
+					Instantiate (barcoEnemigo1, new Vector3 (11, -6.5f, 0), Quaternion.identity);
+				}
+				if (random == 2) {
+					Instantiate (barcoEnemigo2, new Vector3 (-11, -6.5f, 0), Quaternion.identity);
+				}
+				if (random == 3) {
+					Instantiate (barcoEnemigo2, new Vector3 (11, 6.5f, 0), Quaternion.identity);
+				}
+			}
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+		if (!acabado) {
+			if (coll.gameObject.tag == "BarcoEnemigo") {
+				//Bajar una vida
+				Destroy (coll.gameObject);
+				vidas--;
+				textoVidas.text = vidas + " vidas";
+				checkAcabado ();
+			}
+		}
+	}
+
+	void checkAcabado (){
+		if(vidas == 0)
+		{
+			acabado = true;
+			cartel.gameObject.SetActive(true);
+			puntuacionFinal.text = "Has perdido.\n Tu puntuacion ha sido de: " /*+ puntos + "\n El hambre ha disminuido en " + puntos/2*/;
+
+			//PlayerPrefs.SetInt("PuntosBarco", puntos);
+			//PlayerPrefs.SetInt ("Hambre", PlayerPrefs.GetInt ("Hambre") - puntos / 2);
+		}
+	}
+
+	public void aumentarPuntuacion(){
+		puntuacion += 1;
+		textoPuntuacion.text = puntuacion + " puntos";
+
+	} 
+
+}
