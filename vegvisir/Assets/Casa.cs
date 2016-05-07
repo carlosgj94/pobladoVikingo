289c783feb4f14d243246dbf112632478f153001
Carlos Gonzalez Juarez <cargonjua@outlook.com>
2016-05-07 20:48:34 +0200

    Nuevo juego terminado

---
+++ vegvisir/Assets/Casa.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Casa : MonoBehaviour {
+
+    public bool ocupada = false;
+    private float mommentStart;
+    [SerializeField]
+    private Button fotoPareja;
+    [SerializeField]
+    private ParejasController parejas;
+
+    public void crearPareja()
+    {
+        if (!ocupada)
+        {
+            mommentStart = Time.timeSinceLevelLoad;
+            fotoPareja.gameObject.SetActive(true);
+        }
+    }
+
+    public void cerrarPareja(){
+        fotoPareja.gameObject.SetActive(false);
+        ocupada = false;
+        parejas.restarVida();
+    }
+
+    void Update()
+    {
+        if (Time.timeSinceLevelLoad - mommentStart > 1.0f && mommentStart!=0)
+        {       
+            mommentStart = 0;
+            cerrarPareja();
+        }
+    }
+  
+}
+
---
+++ vegvisir/Assets/ParejasController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ParejasController : MonoBehaviour {
+    private float momment;
+    
+    [SerializeField]
+    private List<Casa> listaCasas = new List<Casa>();
+
+    private int points = 0;
+    private int vidas = 3;
+    [SerializeField]
+    private Text puntosTexto;
+    [SerializeField]
+    private Text puntosVida;
+	// Use this for initialization
+	void Start () {
+        momment = -0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (vidas > 0)
+        {
+            if (Time.timeSinceLevelLoad - momment > 0.5f)
+            {
+                momment = Time.timeSinceLevelLoad;
+                int random;
+                do
+                {
+                    random = Random.Range(0, listaCasas.Count);
+                } while (listaCasas[random].ocupada);
+                listaCasas[random].crearPareja();
+
+            }
+        }
+        else
+        {
+            //Aqui se va a liar una gorda!
+        }
+	}
+    public void houseChecked(int number)
+    {
+        listaCasas[number].cerrarPareja();
+        points++;
+        puntosTexto.text = points + " puntos";
+    }
+
+    public void restarVida()
+    {
+        vidas--;
+        puntosVida.text = vidas + " vidas";
+    }
+}
