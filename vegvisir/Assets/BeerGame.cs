6b10fb01aa45880c1b0b112b249254c428a1d81b
Carlos Gonzalez Juarez <cargonjua@outlook.com>
2016-05-07 13:15:13 +0200

    Semiterminado el juego, falta poner el morido, el popup del final

--- vegvisir/Assets/BeerGame.cs
+++ vegvisir/Assets/BeerGame.cs
@@ -14,6 +14,12 @@ public class BeerGame : MonoBehaviour {
 
     [SerializeField]
     private Image barraImagen;
+
+    [SerializeField]
+    private Text puntosTexto;
+
+    private bool morido=false;
+    private int puntos;
    
     // Use this for initialization
     void Start () {
@@ -23,41 +29,53 @@ public class BeerGame : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+        if (Time.time < 10.0f || morido)
         {
-            clicks++;
-            mommentViking = Time.time;
-            this.GetComponent<SpriteRenderer>().sprite = Resources.Load("Beer/" + "borracho02", typeof(Sprite)) as Sprite;
-        }
-        else {
-            if(Time.time-mommentViking > 0.1f)
-                this.GetComponent<SpriteRenderer>().sprite = Resources.Load("Beer/" + "borracho01", typeof(Sprite)) as Sprite;
-        }
+            if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+            {
+                clicks++;
+                mommentViking = Time.time;
+                this.GetComponent<SpriteRenderer>().sprite = Resources.Load("Beer/" + "borracho02", typeof(Sprite)) as Sprite;
+            }
+            else {
+                if (Time.time - mommentViking > 0.1f)
+                    this.GetComponent<SpriteRenderer>().sprite = Resources.Load("Beer/" + "borracho01", typeof(Sprite)) as Sprite;
+            }
 
-        if (Time.time - momment > 1.0f)
+            if (Time.time - momment > 1.0f)
+            {
+                momment = Time.time;
+
+                puntos += clicks;
+
+                if (clicks > 8)
+                {
+                    morido = true;
+                    barraImagen.sprite = Resources.Load("Beer/" + "barracerveza100", typeof(Sprite)) as Sprite;
+                }
+                else if (clicks > 7)
+                    barraImagen.sprite = Resources.Load("Beer/" + "barracerveza85", typeof(Sprite)) as Sprite;
+                else if (clicks > 6)
+                    barraImagen.sprite = Resources.Load("Beer/" + "barracerveza75", typeof(Sprite)) as Sprite;
+                else if (clicks > 5)
+                    barraImagen.sprite = Resources.Load("Beer/" + "barracerveza66", typeof(Sprite)) as Sprite;
+                else if (clicks > 4)
+                    barraImagen.sprite = Resources.Load("Beer/" + "barracerveza50", typeof(Sprite)) as Sprite;
+                else if (clicks > 3)
+                    barraImagen.sprite = Resources.Load("Beer/" + "barracerveza25", typeof(Sprite)) as Sprite;
+                else if (clicks > 2)
+                    barraImagen.sprite = Resources.Load("Beer/" + "barracerveza0", typeof(Sprite)) as Sprite;
+
+                clicks = 0;
+                counterText.text = "Tiempo: " + (10 - (int) Time.time-1);
+                puntosTexto.text = "Puntos: " + puntos;
+
+            }
+        }
+        else
         {
-            momment = Time.time;
-            int total =clicks;
-            
-
-            if(clicks > 8)
-                barraImagen.sprite = Resources.Load("Beer/" + "barracerveza100", typeof(Sprite)) as Sprite;
-            if (clicks >7)
-                barraImagen.sprite = Resources.Load("Beer/" + "barracerveza85", typeof(Sprite)) as Sprite;
-            if (clicks > 6)
-                barraImagen.sprite = Resources.Load("Beer/" + "barracerveza75", typeof(Sprite)) as Sprite;
-            else if(clicks > 5)
-                barraImagen.sprite = Resources.Load("Beer/" + "barracerveza66", typeof(Sprite)) as Sprite;
-            else if(clicks > 4)
-                barraImagen.sprite = Resources.Load("Beer/" + "barracerveza50", typeof(Sprite)) as Sprite;
-            else if(clicks > 3)
-                barraImagen.sprite = Resources.Load("Beer/" + "barracerveza25", typeof(Sprite)) as Sprite;
-            else if(clicks > 2)
-                barraImagen.sprite = Resources.Load("Beer/" + "barracerveza0", typeof(Sprite)) as Sprite;
-
-            clicks = 0;
-            counterText.text = " " + total;
-
+            PlayerPrefs.SetInt("PuntosBeer", puntos);
+            PlayerPrefs.SetInt("Fiesta", PlayerPrefs.GetInt("Fiesta") + puntos / 2);
         }
 	}
 }
