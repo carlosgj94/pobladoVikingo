f4efbf975c610f3b0c052530aaaf2d49b4298f85
Carlos Gonzalez Juarez <cargonjua@outlook.com>
2016-05-07 10:35:27 +0200

    BeerMiniGameScene

---
+++ vegvisir/Assets/BeerGame.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BeerGame : MonoBehaviour {
+
+    private float momment;
+    
+    private int clicks;
+
+    [SerializeField]
+    private Text counterText;
+	// Use this for initialization
+	void Start () {
+        momment = -1.6f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+        {
+            clicks++;
+        }
+
+        if (Time.time - momment > 1.6f)
+        {
+            momment = Time.time;
+            int total =clicks;
+            clicks = 0;
+            counterText.text = " " + total;
+        }
+	}
+}
